d0d4c3c8db36c017fe29bd4de881676a219a50a4
sollall <komugikoakasya@gmail.com>
2023-04-01 18:13:12 +0900

    add graphize in workflows

---
+++ my_module/my_script.py
@@ -0,0 +1,11 @@
+class MyClass:
+    """A simple example class"""         # 三重クォートによるコメント
+
+    def __init__(self):                  # コンストラクタ
+        self.name = ""
+
+    def getName(self):                   # getName()メソッド
+        return self.name
+
+    def setName(self, name):             # setName()メソッド
+        self.name = name
---
+++ my_module/override.py
@@ -0,0 +1,11 @@
+from my_script import MyClass
+
+class Over(MyClass):
+    def __init__(self):
+        self.s="hello"
+
+    def call(self,x):
+        return x
+
+
+
